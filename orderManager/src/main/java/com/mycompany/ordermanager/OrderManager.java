3e85b0ab87dadb9c6c7d83bf1026f99026304b45
svetakosheleva05 <lana.kosheleva05@mail.ru>
2026-03-24 10:59:06 +0300

    Create OrderManager.java

---
+++ orderManager/src/main/java/com/mycompany/ordermanager/OrderManager.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ordermanager;
+
+/**
+ *
+ * @author svetl
+ */
+public class OrderManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        
+        
+    }
+}
